b49b3065796821df672d2896111690020da4b8bd
Wesley Barros <wesleybarros950@gmail.com>
2022-10-10 19:48:42 -0300

    changed to community for better polling

--- index.js
+++ index.js
@@ -1,135 +1,98 @@
-const SteamUser = require("steam-user");
-const SteamCommunity = require("steamcommunity");
-const SteamTotp = require("steam-totp");
-const TradeOfferManager = require("steam-tradeoffer-manager");
-const FS = require("fs");
+const SteamCommunity = require('steamcommunity')
+const SteamTotp = require('steam-totp')
+const TradeOfferManager = require('steam-tradeoffer-manager')
+const FS = require('fs')
 
-const config = require("./config.json");
+const config = require('./config.json')
 
-let atomicCounter = 0;
-
-let client = new SteamUser();
-let manager = new TradeOfferManager({
-  steam: client,
-  domain: config.domainName,
-  language: "en",
-});
 let community = new SteamCommunity();
+let manager = new TradeOfferManager({
+  domain: "example.com",
+  language: "en",
+  pollInterval: 60000
+})
 
 let logOnOptions = {
   accountName: config.username,
   password: config.password,
-  twoFactorCode: SteamTotp.getAuthCode(config.apiKey),
-};
-
-if (FS.existsSync("polldata.json")) {
-  manager.pollData = JSON.parse(
-    FS.readFileSync("polldata.json").toString("utf8")
-  );
+  twoFactorCode: SteamTotp.getAuthCode(config.apiKey)
 }
 
-client.logOn(logOnOptions);
+if (FS.existsSync("steamguard.txt"))
+{
+  logOnOptions.stemguard = FS.readFileSync("steamguard.txt").toString("utf8")
+}
 
-client.on("loggedOn", () => {
-  console.log("Logged into Steam");
+if (FS.existsSync("polldata.txt")) {
+  manager.pollData = JSON.parse(FS.readFileSync("polldata.json").toString("utf8"))
+}
 
-  setInterval(() => {
-    if (atomicCounter > 0) {
-      console.log('Cannot update cookies because there are trades waiting to be accepted.')
-      return
-    } else {
-      console.log("WARNING: Updating session.");
-      client.webLogOn();
-    }
-  }, 60 * 60 * 1000);
-});
-
-client.on("webSession", (sessionID, cookies) => {
-  manager.setCookies(cookies, (err) => {
-    if (err) {
-      console.log(err);
-      process.exit(1);
-    }
-
-    console.log("Got API key: " + manager.apiKey);
-  });
-
-  community.setCookies(cookies);
-});
-
-community.on("sessionExpired", (err) => {
+community.login(logOnOptions, (err, sessionID, cookies, steamguard) => {
   if (err) {
-    console.log(`WARNING: Session expired: ${err}`);
+    console.log(err)
+    process.exit(1)
   }
 
-  if (client.steamID) {
-    client.webLogOn();
-    console.log("Re-logging on using client.webLogOn().");
-  } else {
-    client.logOn(logOnOptions);
-    console.log("Re-logging on using client.logOn().");
-  }
-});
+  console.log(`Got API key: ${manager.apiKey}`)
+})
 
 manager.on("newOffer", (offer) => {
-  console.log(
-    `New offer #${offer.id} from ${offer.partner.getSteam3RenderedID()}`
-  );
-  let will_accept = offer.itemsToGive.length == 0;
-  if (will_accept) {
-    setTimeout(() => {
-      offer.accept((err, status) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("Offer accepted: " + status);
-          if (status == "pending") {
-            community.acceptConfirmationForObject(
-              "identitySecret",
-              offer.id,
-              (err) => {
-                if (err) {
-                  console.log("Can't confirm trade offer: " + err.message);
-                } else {
-                  console.log("Trade offer " + offer.id + " confirmed");
-                }
-              }
-            );
+  console.log(`New offer #${offer.id} from ${offer.partner.getSteam3RenderedID()}`)
+  offer.accept((err, status) => {
+    if (err) {
+      console.log(`Unable to accept offer: ${err.message}`)
+    }
+    else {
+      console.log(`Offer accepted: ${status}`)
+      if (status == "pending") {
+        community.acceptConfirmationForObject("identitySecret", offer.id, (err) => {
+          if (err) {
+            console.log(`Can't confirm trade offer: ${err.message}`)
           }
-        }
-      });
-      atomicCounter -= 1
-    }, 60000);
-    atomicCounter += 1
-  }
-});
+          else {
+            console.log(`Trade offer ${offer.id} confirmed`)
+          }
+        })
+      }
+    }
+  })
+})
 
 manager.on("receivedOfferChanged", (offer, oldState) => {
-  console.log(
-    `Offer #${offer.id} changed: ${
-      TradeOfferManager.ETradeOfferState[oldState]
-    } -> ${TradeOfferManager.ETradeOfferState[offer.state]}`
-  );
-
+  console.log(`Offer #${offer.id} changed: ${TradeOfferManager.ETradeOfferState[oldState]} -> ${TradeOfferManager.ETradeOfferState[offer.state]}`)
   if (offer.state == TradeOfferManager.ETradeOfferState.Accepted) {
-    offer.getExchangeDetails(
-      (err, status, tradeInitTime, receivedItems, sentItems) => {
-        if (err) {
-          console.log(`Error ${err}`);
-          return;
-        }
-
-        let newReceivedItems = receivedItems.map((item) => item.new_assetid);
-        console.log(
-          `Received items ${newReceivedItems.join(",")} - status ${
-            TradeOfferManager.ETradeStatus[status]
-          }`
-        );
+    offer.getExchangeDetails((err, status, tradeInitTime, receivedItems, sentItems) => {
+      if (err) {
+        console.log(`Error: ${err}`)
+        return
       }
-    );
-  }
-});
 
-manager.on("pollData", (pollData) => {
-  FS.writeFileSync("polldata.json", JSON.stringify(pollData));
-});
+      let newReceivedItemss = receivedItems.map(item => item.new_assetid)
+      console.log(`Received items: ${newReceivedItemss}`)
+    })
+  }
+})
+
+manager.on('pollData', (pollData) => {
+  FS.writeFileSync('polldata.json', JSON.stringify(pollData))
+})
+
+const renewSession = () => {
+  console.log("Warning: Renewing session...")
+  community.login(logOnOptions, (err, sessionID, cookies, steamguard) => {
+    if (err) {
+      console.log(err)
+      process.exit(1)
+    }
+  
+    console.log(`Got API key: ${manager.apiKey}`)
+  })
+}
+
+community.on("sessionExpired", () => {
+  renewSession()
+})
+
+setTimeout(() => {
+  renewSession()
+}, 60 * 60 * 1000);
